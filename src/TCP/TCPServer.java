6f00abedde9b8d2f2789d89dd028f9987539092b
${Gabriel Velloso} <${g.velloso@edu.pucrs.br}>
2022-09-18 17:17:12 -0300

    first commit

---
+++ src/TCP/TCPServer.java
@@ -0,0 +1,45 @@
+package TCP;
+
+import java.io.*;
+import java.net.*;
+ 
+// Recebe uma mensagem de algum cliente
+// Imprime mensagem na tela
+
+public class TCPServer {
+ 
+    private static DataOutputStream dataOutputStream = null;
+    private static DataInputStream dataInputStream = null;
+    public static void main(String[] args) {
+
+        try(ServerSocket serverSocket = new ServerSocket(5000)){
+            System.out.println("listening to port:5000");
+            Socket clientSocket = serverSocket.accept();
+            System.out.println(clientSocket+" connected.");
+            dataInputStream = new DataInputStream(clientSocket.getInputStream());
+            dataOutputStream = new DataOutputStream(clientSocket.getOutputStream());
+
+            receiveFile("ArquivoMin.txt");
+            receiveFile("ArquivoMax.txt");
+
+            dataInputStream.close();
+            dataOutputStream.close();
+            clientSocket.close();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    private static void receiveFile(String fileName) throws Exception{
+        int bytes = 0;
+        FileOutputStream fileOutputStream = new FileOutputStream(fileName);
+        
+        long size = dataInputStream.readLong();     // read file size
+        byte[] buffer = new byte[4*1024];
+        while (size > 0 && (bytes = dataInputStream.read(buffer, 0, (int)Math.min(buffer.length, size))) != -1) {
+            fileOutputStream.write(buffer,0,bytes);
+            size -= bytes;      // read upto file size
+        }
+        fileOutputStream.close();
+    }
+}
